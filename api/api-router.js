a93c07dd3aa0e6b602619b93ae431f4c89bf8b7d
Sierra Curtis <sierracurtis91@gmail.com>
2020-02-24 18:23:37 -0500

    post new user is working

--- api/api-router.js
+++ api/api-router.js
@@ -2,14 +2,22 @@ const bcrypt = require('bcryptjs');
 const router = require('express').Router();
 
 const authRouter = require('../auth/auth-router');
-const usersRouter = require();
-const restricted = require();
+const usersRouter = require('../users/users-router');
+const restricted = require('../auth/restricted-middleware');
 
 router.use('/auth', authRouter);
 router.use('/users', restricted, usersRouter);
 
+router.get('/hash', (req, res) => {
+    const authentication = req.headers.authentication;
+
+    const hash = bcrypt.hashSync(authentication, 8)
+
+    res.json({ originalValue: authentication, hashValue: hash});
+});
+
 router.get('/', (req, res) => {
     res.json({ api: 'App is working' });
-})
+});
 
 module.exports = router;
---
+++ auth/auth-router.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcryptjs');
+const router = require('express').Router();
+
+const Users = require('../users/users-model');
+
+router.post('/register', (req, res) => {
+    let user = req.body;
+
+    const hash = bcrypt.hashSync(user.password, 8);
+
+    user.password = hash;
+
+    Users.add(user)
+    .then(saved => {
+        res.status(201).json(saved);
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    })
+});
+
+module.exports = router;
