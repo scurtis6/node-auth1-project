90236fbe4aeb5190cf30ad80ab853063e611fec3
Sierra Curtis <sierracurtis91@gmail.com>
2020-02-25 17:50:37 -0500

    fixed login issue

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -19,4 +19,22 @@ router.post('/register', (req, res) => {
     })
 });
 
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+    .first()
+    .then(user => {
+        console.log('login', user)
+        if (user && bcrypt.compareSync(password, user.password)) {
+            res.status(200).json({ message: `You are logged in ${user.username}!!!` })
+        } else {
+            res.status(401).json({ message: 'You shall not pass!' })
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
+
 module.exports = router;
